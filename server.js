5df24c71ff0efdae26e29811c3434425aa5d12e3
Helio Alves <helios25@gmail.com>
2017-02-27 15:40:59 -0500

    Added projects

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    welcomeMessage: 'Some projects I\'ve worked on'
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'There was a problem with your request'
